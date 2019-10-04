5e17a141336adae7a85e2461cf0eff82f26ef59b
Nikita Vasilevsky <nvasilevskiy@zoon.ru>
2019-10-04 09:36:27 +0300

    dockerfile preparation

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -36,4 +36,23 @@ else
     make_user "admin@vpnadmin.local" $pwd
 fi
 
-service nginx start && service php7.2-fpm start && tail -f /var/log/nginx/access.log
+# enable IP forwarding
+sysctl -w net.ipv4.ip_forward=1
+
+# configure firewall
+iptables -t nat -A POSTROUTING -s 172.16.10.0/24 ! -d 172.16.10.0/24 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 10.168.1.0/24 ! -d 10.168.1.0/24 -j MASQUERADE
+iptables -A FORWARD -s 172.16.10.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
+iptables -A FORWARD -s 10.168.1.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
+iptables -A INPUT -i ppp+ -j ACCEPT
+iptables -A OUTPUT -o ppp+ -j ACCEPT
+iptables -A FORWARD -i ppp+ -j ACCEPT
+iptables -A FORWARD -o ppp+ -j ACCEPT
+
+
+service nginx start \
+&& service php7.2-fpm start \
+&& service ipsec start \
+&& service xl2tpd start \
+&& service pptpd start \
+&& service && tail -f /var/log/nginx/access.log
