fa014cdff80cfe24169c00b9d1101ccba1a8f07c
Nikita Vasilevsky <nvasilevskiy@zoon.ru>
2019-10-29 14:21:53 +0300

    env add

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -38,7 +38,7 @@ fi
 
 # enable IP forwarding
 sysctl -w net.ipv4.ip_forward=1
-
+modprobe af_key
 # configure firewall
 iptables -t nat -A POSTROUTING -s 172.16.10.0/24 ! -d 172.16.10.0/24 -j MASQUERADE
 iptables -t nat -A POSTROUTING -s 10.168.1.0/24 ! -d 10.168.1.0/24 -j MASQUERADE
