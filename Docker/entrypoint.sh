04f491bfb4613d262530ca41373b55704995c95d
Nikita Vasilevsky <nvasilevskiy@zoon.ru>
2020-01-19 10:28:44 +0300

    reworking docker module

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -35,6 +35,163 @@ else
     echo "generated PASSWORD: $pwd"
     make_user "admin@vpnadmin.local" $pwd
 fi
+if [[ -z "$PUBLIC_IP" ]]; then
+	PUBLIC_IP=$(wget -t 3 -T 15 -qO- http://ipv4.icanhazip.com)
+fi
+
+
+
+L2TP_NET='10.100.100.0/24'
+L2TP_LOCAL='10.100.100.1'
+L2TP_POOL='10.100.100.10-10.100.100.254'
+PPTP_NET='10.100.101.0/24'
+PPTP_LOCAL='10.100.101.1'
+PPTP_POOL='10.100.101.10-10.100.101.254'
+DNS_SRV1='8.8.8.8'
+DNS_SRV2='8.8.4.4'
+DNS_SRVS="\"$DNS_SRV1 $DNS_SRV2\""
+SHA2_TRUNCBUG='no'
+
+
+# Create IPsec (Libreswan) config
+cat > /etc/ipsec.conf <<EOF
+version 2.0
+config setup
+  virtual-private=%v4:192.168.0.0/16,%v4:172.16.0.0/12,%v4:!$L2TP_NET,%v4:!$XAUTH_NET
+  protostack=netkey
+  interfaces=%defaultroute
+  uniqueids=no
+conn shared
+  left=%defaultroute
+  leftid=$PUBLIC_IP
+  right=%any
+  encapsulation=yes
+  authby=secret
+  pfs=no
+  rekey=no
+  keyingtries=5
+  dpddelay=30
+  dpdtimeout=120
+  dpdaction=clear
+  ikev2=never
+  ike=aes256-sha2,aes128-sha2,aes256-sha1,aes128-sha1,aes256-sha2;modp1024,aes128-sha1;modp1024
+  phase2alg=aes_gcm-null,aes128-sha1,aes256-sha1,aes256-sha2_512,aes128-sha2,aes256-sha2
+  sha2-truncbug=$SHA2_TRUNCBUG
+conn l2tp-psk
+  auto=add
+  leftprotoport=17/1701
+  rightprotoport=17/%any
+  type=transport
+  phase2=esp
+  also=shared
+EOF
+
+if uname -r | grep -qi 'coreos'; then
+  sed -i '/phase2alg/s/,aes256-sha2_512//' /etc/ipsec.conf
+fi
+
+# Create xl2tpd config
+cat > /etc/xl2tpd/xl2tpd.conf <<EOF
+[global]
+port = 1701
+[lns default]
+ip range = $L2TP_POOL
+local ip = $L2TP_LOCAL
+require chap = yes
+refuse pap = yes
+require authentication = yes
+name = l2tpd
+pppoptfile = /etc/ppp/options.xl2tpd
+length bit = yes
+EOF
+
+# Set xl2tpd options
+cat > /etc/ppp/options.xl2tpd <<EOF
++mschap-v2
+ipcp-accept-local
+ipcp-accept-remote
+noccp
+auth
+mtu 1280
+mru 1280
+proxyarp
+lcp-echo-failure 4
+lcp-echo-interval 30
+connect-delay 5000
+ms-dns $DNS_SRV1
+EOF
+
+cat > /etc/ppp/options.pptpd <<EOF
+name pptpd
+refuse-pap
+refuse-chap
+refuse-mschap
+require-mschap-v2
+require-mppe-128
+ms-dns $DNS_SRV1
+proxyarp
+lock
+nobsdcomp
+novj
+novjccomp
+nologfd
+EOF
+
+cat > /etp/pptpd.conf <<EOF
+option /etc/ppp/options.pptpd
+logwtmp
+localip $PPTP_LOCAL
+remoteip $PPTP_POOL
+EOF
+if [ -z "$VPN_DNS_SRV1" ] || [ -n "$VPN_DNS_SRV2" ]; then
+cat >> /etc/ppp/options.xl2tpd <<EOF
+ms-dns $DNS_SRV2
+EOF
+fi
+
+# Update sysctl settings
+SYST='/sbin/sysctl -e -q -w'
+if [ "$(getconf LONG_BIT)" = "64" ]; then
+  SHM_MAX=68719476736
+  SHM_ALL=4294967296
+else
+  SHM_MAX=4294967295
+  SHM_ALL=268435456
+fi
+$SYST kernel.msgmnb=65536
+$SYST kernel.msgmax=65536
+$SYST kernel.shmmax=$SHM_MAX
+$SYST kernel.shmall=$SHM_ALL
+$SYST net.ipv4.ip_forward=1
+$SYST net.ipv4.conf.all.accept_source_route=0
+$SYST net.ipv4.conf.all.accept_redirects=0
+$SYST net.ipv4.conf.all.send_redirects=0
+$SYST net.ipv4.conf.all.rp_filter=0
+$SYST net.ipv4.conf.default.accept_source_route=0
+$SYST net.ipv4.conf.default.accept_redirects=0
+$SYST net.ipv4.conf.default.send_redirects=0
+$SYST net.ipv4.conf.default.rp_filter=0
+$SYST net.ipv4.conf.eth0.send_redirects=0
+$SYST net.ipv4.conf.eth0.rp_filter=0
+
+# Create IPTables rules
+iptables -I INPUT 1 -p udp --dport 1701 -m policy --dir in --pol none -j DROP
+iptables -I INPUT 2 -m conntrack --ctstate INVALID -j DROP
+iptables -I INPUT 3 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -I INPUT 4 -p udp -m multiport --dports 500,4500 -j ACCEPT
+iptables -I INPUT 5 -p udp --dport 1701 -m policy --dir in --pol ipsec -j ACCEPT
+iptables -I INPUT 6 -p udp --dport 1701 -j DROP
+iptables -I FORWARD 1 -m conntrack --ctstate INVALID -j DROP
+iptables -I FORWARD 2 -i eth+ -o ppp+ -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -I FORWARD 3 -i ppp+ -o eth+ -j ACCEPT
+iptables -I FORWARD 4 -i ppp+ -o ppp+ -s "$L2TP_NET" -d "$L2TP_NET" -j ACCEPT
+iptables -I FORWARD 4 -i ppp+ -o ppp+ -s "$PPTP_NET" -d "$PPTP_NET" -j ACCEPT
+iptables -A FORWARD -j DROP
+iptables -t nat -I POSTROUTING -s "$PPTP_POOL" -o eth+ -j MASQUERADE
+iptables -t nat -I POSTROUTING -s "$L2TP_NET" -o eth+ -j MASQUERADE
+
+# Update file attributes
+chmod 600 /etc/ipsec.secrets /etc/ppp/chap-secrets /etc/ipsec.d/passwd
 
 # enable IP forwarding
 sysctl -w net.ipv4.ip_forward=1
@@ -52,8 +209,10 @@ iptables -A FORWARD -o ppp+ -j ACCEPT
 
 service nginx start \
 && service php7.2-fpm start \
-&& service ipsec start \
-&& service xl2tpd start \
 && service pptpd start \
 && service rsyslog start \
+&& mkdir -p /run/pluto /var/run/pluto /var/run/xl2tpd \
+&& rm -f /run/pluto/pluto.pid /var/run/pluto/pluto.pid /var/run/xl2tpd.pid \
+&& /usr/local/sbin/ipsec start \
+&& exec /usr/sbin/xl2tpd -D -c /etc/xl2tpd/xl2tpd.conf \
 && tail -f /var/log/nginx/access.log
